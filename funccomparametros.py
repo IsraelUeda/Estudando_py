ebcbaa05eae30713122328000a5495fe8b724d85
IsraelUeda <israelueda@gmail.com>
2024-09-17 17:30:30 -0300

    módulos costumizados

--- funccomparametros.py
+++ funccomparametros.py
@@ -100,3 +100,16 @@ print (soma_impares(lista))
 #return finaliza a função e não terminara o codigo
 
 """ 
+
+def soma_impares(numeros):
+    total = 0
+    for num in numeros:
+        if num % 2 != 0:
+            total = total + num
+    return total
+
+lista = [1, 2, 3, 4, 5, 6, 7]
+
+print (soma_impares(lista))
+
+#return finaliza a função e não terminara o codigo
---
+++ módulos_costumizados.py
@@ -0,0 +1,18 @@
+"""
+Podemos criar nossos próprios módulos
+
+Como módulos python são arquivos python ent todos os programas
+ou arquivos que criamos nesse curso estão pronto para serem utilizados
+
+#importando uma função especifica do moodulo
+from funccomparametros import soma_impares as si
+
+print(si([1,2,3,45,6,]))
+
+import funccomparametros as fcp#temos acesso a todos os elementos do módulo
+
+#estamos acessando e imprimindo uma váriavel do módulo
+print(fcp.lista)
+
+print(fcp.soma_impares(fcp.lista))
+"""
